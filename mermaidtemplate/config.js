aa7e0d4ca1db88e32a4440334c121a1ecb6ee0d3
Jordan Hiertz <jordan.hiertz@al-enterprise.com>
2017-11-16 11:27:10 +0100

    Add output to configuration file

--- mermaidtemplate/config.js
+++ mermaidtemplate/config.js
@@ -1,4 +1,5 @@
 module.exports = {
+    output: "./", // Specify the path to the mermaid output file
     visibility: "public", // Can be "all", "private", "public"
     enum: false, // Parse and add enumeration to doc
     allowUndefinedType: false,
