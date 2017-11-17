780d0b21918b5a4ac03df852a6340167545361a9
Jordan Hiertz <jordan.hiertz@al-enterprise.com>
2017-11-17 09:52:44 +0100

    change output destination

--- mermaidtemplate/config.js
+++ mermaidtemplate/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    output: "./", // Specify the path to the mermaid output file
+    output: "./bin/jsdoc/sheets/", // Specify the path to the mermaid output file
     visibility: "public", // Can be "all", "private", "public"
     enum: false, // Parse and add enumeration to doc
     allowUndefinedType: false,
